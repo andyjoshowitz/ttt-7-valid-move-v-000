4c1f1c87c5a537ccc7367d85791b93973ebaee86
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-01-25 18:41:40 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,5 +9,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  !position_taken?(board, index) && index.between?(0, 8)
+  position_taken?(board, index) && index.between?(0, 8)
 end
